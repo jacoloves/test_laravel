2ae348b35169153ea328655b7e44995ef46aaeff
Shotaro Tanaka <5511068t@gmail.com>
2018-10-16 22:46:22 +0900

    20181016

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('board', 'BoardController@index');
 Route::get('board/add', 'BoardController@add');
 Route::post('board/add', 'BoardController@create'); 
 
+Route::get('person', 'PersonController@index');
 /*
 Route::get('/', function () {
     return view('welcome');
