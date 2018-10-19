1c4f440ce9787c24db6308817bbef75d665e0871
Shotaro Tanaka <5511068t@gmail.com>
2018-10-20 07:57:47 +0900

    20181020

--- app/Person.php
+++ app/Person.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use App\Scopes\ScopePerson;
 
 class Person extends Model
 {
@@ -31,8 +32,14 @@ class Person extends Model
     {
         parent::boot();
 
-        static::addGlobalScope('age', function (Builder $builder) {
-            $builder->where('age', '>', 20);
-        });
+        static::addGlobalScope(new ScopePerson);
     }
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'mail' => 'email',
+        'age' => 'integer|min:0|max:150'
+    );
 }
